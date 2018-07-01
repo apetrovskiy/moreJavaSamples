48baa804c686d19da55ddec5017f7a3105c97885
Alexander Petrovskiy <alexander.petrovskiy@aurea.com>
2018-07-01 21:12:59 +0300

    .

--- restTemplate/src/test/java/com/test/spring/Test001.java
+++ restTemplate/src/test/java/com/test/spring/Test001.java
@@ -1,7 +1,15 @@
 package com.test.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Test001 {
     public static final String BASE_URL = "http://localhost:1235/v2";
@@ -9,16 +17,9 @@ public class Test001 {
 
     @Test
     public void testConnect() {
-        // http://localhost:1235/v2/cats?name=barsik
-//        Response response = given()
-//                .formParam("name", "barsik")
-//                // .header("aa", "dd")
-//                .when().get(BASE_URL + CATS);
-//        response.then().assertThat().statusCode(200);
-
         RestTemplate restTemplate = new RestTemplate();
-        // restTemplate.
-
-        // System.out.println(response);
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + CATS + "?name=barsik", String.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        System.out.print(response.getBody());
     }
 }
