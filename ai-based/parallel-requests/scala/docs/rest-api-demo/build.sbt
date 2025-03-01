10e2658b9307847c4ba8d06c3c1288f04ef9a4ee
Alexander Petrovskiy <uiautomation@hotmail.com>
2025-03-01 22:11:15 +0300

    this is working

--- ai-based/parallel-requests/scala/docs/rest-api-demo/build.sbt
+++ ai-based/parallel-requests/scala/docs/rest-api-demo/build.sbt
@@ -7,19 +7,9 @@ libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % "1.5.17", // Logging
   "org.scalatest" %% "scalatest" % "3.2.15" % Test // Testing
 )
-// libraryDependencies += "com.github.javafaker" % "javafaker" % "1.0.2"
 libraryDependencies += "net.datafaker" % "datafaker" % "2.4.2"
-// libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.5.17"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % Test
-// libraryDependencies += "com.github.tomakehurst" % "wiremock" % "2.35.0" % Test
-// https://mvnrepository.com/artifact/com.github.tomakehurst/wiremock
-// libraryDependencies += "com.github.tomakehurst" % "wiremock" % "2.27.2" % Test pomOnly()
-// libraryDependencies += "org.wiremock" % "wiremock" % "3.12.0" % Test
 libraryDependencies += "org.wiremock" % "wiremock" % "3.12.0" % Test
-// libraryDependencies += "com.h2database" % "h2" % "2.1.214"
-// libraryDependencies += "com.h2database" % "h2" % "2.3.232" % Test
-// https://mvnrepository.com/artifact/pl.droidsonroids.yaml/snakeyaml
-// libraryDependencies += "pl.droidsonroids.yaml" % "snakeyaml" % "1.18.2" % Test
-// libraryDependencies += "org.yaml" % "snakeyaml" % "2.4" % Test
+libraryDependencies += "com.h2database" % "h2" % "2.3.232" % Test
 libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.18.2"
 
--- ai-based/parallel-requests/scala/docs/rest-api-demo/src/test/scala/com/example/mock/UserRepository.scala
+++ ai-based/parallel-requests/scala/docs/rest-api-demo/src/test/scala/com/example/mock/UserRepository.scala
@@ -1,58 +1,59 @@
-// package com.example.mock
+package com.example.mock
 
-// import com.example.model.User
-// import java.sql.{Connection, DriverManager, PreparedStatement}
+import com.example.model.User
+import java.sql.{Connection, DriverManager, PreparedStatement}
+import java.util.UUID
 
-// class UserRepository {
-//   private val url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
-//   private val connection: Connection = DriverManager.getConnection(url)
+class UserRepository {
+  private val url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
+  private val connection: Connection = DriverManager.getConnection(url)
 
-//   // Create users table
-//   private val createTableSQL =
-//     """
-//       |CREATE TABLE users (
-//       |  id UUID PRIMARY KEY,
-//       |  firstName VARCHAR(255),
-//       |  lastName VARCHAR(255),
-//       |  age INT,
-//       |  position VARCHAR(255)
-//       |""".stripMargin
+  // Create users table
+  private val createTableSQL =
+    """
+      |CREATE TABLE users (
+      |  id UUID PRIMARY KEY,
+      |  firstName VARCHAR(255),
+      |  lastName VARCHAR(255),
+      |  age INT,
+      |  position VARCHAR(255)
+      |""".stripMargin
 
-//   private val statement = connection.createStatement()
-//   statement.execute(createTableSQL)
+  private val statement = connection.createStatement()
+  statement.execute(createTableSQL)
 
-//   def saveUser(user: User): Unit = {
-//     val insertSQL =
-//       """
-//         |INSERT INTO users (id, firstName, lastName, age, position)
-//         |VALUES (?, ?, ?, ?, ?)
-//         |""".stripMargin
+  def saveUser(user: User): Unit = {
+    val insertSQL =
+      """
+        |INSERT INTO users (id, firstName, lastName, age, position)
+        |VALUES (?, ?, ?, ?, ?)
+        |""".stripMargin
 
-//     val preparedStatement: PreparedStatement = connection.prepareStatement(insertSQL)
-//     preparedStatement.setString(1, user.id.toString)
-//     preparedStatement.setString(2, user.firstName)
-//     preparedStatement.setString(3, user.lastName)
-//     preparedStatement.setInt(4, user.age)
-//     preparedStatement.setString(5, user.position)
-//     preparedStatement.executeUpdate()
-//   }
+    val preparedStatement: PreparedStatement = connection.prepareStatement(insertSQL)
+    preparedStatement.setString(1, user.id.toString)
+    preparedStatement.setString(2, user.firstName)
+    preparedStatement.setString(3, user.lastName)
+    preparedStatement.setInt(4, user.age)
+    preparedStatement.setString(5, user.position)
+    preparedStatement.executeUpdate()
+  }
 
-//   def getUserById(id: String): Option[User] = {
-//     val selectSQL = "SELECT * FROM users WHERE id = ?"
-//     val preparedStatement: PreparedStatement = connection.prepareStatement(selectSQL)
-//     preparedStatement.setString(1, id)
-//     val resultSet = preparedStatement.executeQuery()
+  def getUserById(id: String): Option[User] = {
+    val selectSQL = "SELECT * FROM users WHERE id = ?"
+    val preparedStatement: PreparedStatement = connection.prepareStatement(selectSQL)
+    preparedStatement.setString(1, id)
+    val resultSet = preparedStatement.executeQuery()
 
-//     if (resultSet.next()) {
-//       Some(User(
-//         id = UUID.fromString(resultSet.getString("id")),
-//         firstName = resultSet.getString("firstName"),
-//         lastName = resultSet.getString("lastName"),
-//         age = resultSet.getInt("age"),
-//         position = resultSet.getString("position")
-//       ))
-//     } else {
-//       None
-//     }
-//   }
-// }
+    if (resultSet.next()) {
+      Some(User(
+        id = UUID.fromString(resultSet.getString("id")),
+        firstName = resultSet.getString("firstName"),
+        lastName = resultSet.getString("lastName"),
+        age = resultSet.getInt("age"),
+        position = resultSet.getString("position")
+      ))
+    } else {
+      None
+    }
+  }
+}
